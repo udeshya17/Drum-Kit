cb2c443c85db0d9a3abee564c34a2fe3fb868501
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2023-04-16 01:48:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+
+    // this.style.color = "white";
+    var buttonInnerHTML = this.innerHTML;
+
+    makesound(buttonInnerHTML);
+
+    buttonAnimattion(buttonInnerHTML);
+
+  });
+
+}
+
+
+  document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonAnimattion(event.key);
+
+  });
+
+
+function makesound(key){
+  switch (key) {
+    case "w":
+    var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+      break;
+
+      case "a":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+        break;
+
+        case "s":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+          break;
+
+          case "d":
+          var audio = new Audio('sounds/snare.mp3');
+          audio.play();
+            break;
+
+            case "j":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+              break;
+
+              case "k":
+              var audio = new Audio('sounds/crash.mp3');
+              audio.play();
+                break;
+
+                case "l":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                  break;
+
+    default: console.log(buttonInnerHTML);s
+
+  }
+}
+
+
+
+
+function buttonAnimattion(currentKey){
+ var activeKey = document.querySelector("." + currentKey);
+
+ activeKey.classList.add("pressed");
+
+ setTimeout(function(){
+  activeKey.classList.remove("pressed");
+
+},100);
+}
